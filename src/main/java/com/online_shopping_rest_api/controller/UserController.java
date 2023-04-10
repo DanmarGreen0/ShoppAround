cd3a3358ea9f4e31e401b21851c5a69497f5fdd5
Danmar <danmargreen)@gmail.com>
2023-04-09 18:52:38 -0700

    *delete merging code line

--- src/main/java/com/online_shopping_rest_api/controller/UserController.java
+++ src/main/java/com/online_shopping_rest_api/controller/UserController.java
@@ -81,11 +81,7 @@ public class UserController {
     }
 
     @GetMapping(value = { "/user", "/user/{id}" })
-<<<<<<< HEAD
-    @PreAuthorize("hasAnyRole('ROLE_MASTER_ADMIN', 'ROLE_ADMIN')")
-=======
     // @PreAuthorize("hasAnyRole('ROLE_MASTER_ADMIN', 'ROLE_ADMIN')")
->>>>>>> webappIntegration
     public ResponseEntity<Object> getUserAccountBy(@PathVariable Optional<Integer> id,
             @RequestParam Optional<String> username) {
 
@@ -104,13 +100,8 @@ public class UserController {
                 HttpStatus.OK);
     }
 
-<<<<<<< HEAD
-    @PostMapping(path = "/admin/user", produces = "application/json")
-    @PreAuthorize("hasRole('ROLE_MASTER_ADMIN')")
-=======
     @PostMapping(path = "/user", produces = "application/json")
     // @PreAuthorize("hasRole('ROLE_MASTER_ADMIN')")
->>>>>>> webappIntegration
     public ResponseEntity<Object> addAdminUser(@RequestBody UserDTO userDTO) {
 
         final Object userJson = mapUserEntity.toJson(userService.createAdminUser(userDTO));
